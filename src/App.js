888100090b26c4363a41d2fdce6290177bdd36fb
Nivetha Jayakumar <nivethajayakumar5@gmail.com>
2022-04-28 20:14:42 +0530

    test-checkbox

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import { Checkbox } from '@momentum-ui/react';
+import '@momentum-ui/core/css/momentum-ui.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Checkbox
+          value='mute-video-im'
+          name='meeting-controls'
+          label='mute-video'
+          htmlId='testCheckbox2'
+          checked={true}   
+      />   
       </header>
     </div>
   );
